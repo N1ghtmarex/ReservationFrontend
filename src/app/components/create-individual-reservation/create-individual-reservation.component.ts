1f65a0475334c05fb1bb2a09c0cb4da83609daff
andre <andreyshe22@gmail.com>
2023-07-13 19:53:43 +0500

    выбор спорта через список

--- src/app/components/create-individual-reservation/create-individual-reservation.component.ts
+++ src/app/components/create-individual-reservation/create-individual-reservation.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { IndividualReservation } from 'src/app/models/addIndividualReservationDto';
+import { ISport } from 'src/app/models/sport';
 import { IndividualReservationsService } from 'src/app/services/individual-reservations.service'
+import { SportsService } from 'src/app/services/sports.service';
 
 @Component({
   selector: 'app-create-individual-reservation',
@@ -11,12 +13,15 @@ import { IndividualReservationsService } from 'src/app/services/individual-reser
 export class CreateIndividualReservationComponent implements OnInit {
   form!: FormGroup
 
+  sports!: ISport[]
   reservation = new IndividualReservation();
 
-  constructor(private reservationService: IndividualReservationsService){
+  constructor(private reservationService: IndividualReservationsService,
+              private sportService: SportsService){
   }
 
   ngOnInit(): void {
+    this.getSports()
     this.form = new FormGroup({
       date: new FormControl(null, [Validators.required]),
       duration: new FormControl(null, [Validators.required]),
@@ -27,5 +32,11 @@ export class CreateIndividualReservationComponent implements OnInit {
   addReservation(reservation: IndividualReservation) {
     this.reservationService.addReservation(reservation).subscribe(() => {
     });
+  }
+
+  getSports() {
+    this.sportService.getSports().subscribe((any) => {
+      this.sports = any.sports
+    })
   }
 }
--- src/app/models/addIndividualReservationDto.ts
+++ src/app/models/addIndividualReservationDto.ts
@@ -1,5 +1,5 @@
 export class IndividualReservation {
     reservationDate = '';
     duration = '';
-    sportName = '';
+    sportId = '';
 }
